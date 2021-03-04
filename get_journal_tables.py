3fe6f261116bf685a9b465411570ac8b9e44160b
mstalbot <34695056+mstalbot@users.noreply.github.com>
2021-03-04 13:44:31 -0700

    Update get_journal_tables.py

--- get_journal_tables.py
+++ get_journal_tables.py
@@ -945,7 +945,10 @@ class Journal_tables():
             else: new_value = map[mkey]
             map[mkey] = new_value
             print('>>>>', mkey, map[mkey], new_value)
-            if table_row[new_value.split('@')[0] if '@' in str(new_value) else new_value] is not None: empty = False
+            try:
+                if table_row[new_value.split('@')[0] if '@' in str(new_value) else new_value] is not None: empty = False
+            except:
+                if table_row[int(new_value.split('@')[0]) if '@' in str(new_value) else int(new_value)] is not None: empty = False
         if empty: print('Skipping row since empty')
         else:
             #Convert to a standardized format
