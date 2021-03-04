dfcd3c91cc8fa3dba3a5acb4acfe491b6734ae90
mstalbot <34695056+mstalbot@users.noreply.github.com>
2021-03-03 18:36:31 -0700

    Update get_journal_tables.py

--- get_journal_tables.py
+++ get_journal_tables.py
@@ -1197,12 +1197,10 @@ class Journal_tables():
                 self.lens_objects[standard_name]['z_Lens'].append({'value': row[3], 'tracer': {'update status': 'in SuGOHI', 'weight':5}})
                 self.lens_objects[standard_name]['z_Source(s)'].append({'value': row[4], 'tracer': {'update status': 'in SuGOHI', 'weight':5}})
                 self.lens_objects[standard_name]['System Name'].append({'value': 'HSC'+standard_name, 'tracer': {'update status': 'in SuGOHI', 'weight':5}})
-                for sr in sugohi_key[row[-1]].split(' '):
-                    if 'SuGOHI' in sr:
-                        reft = sr
-                        break
-                if 'References' in self.lens_objects[standard_name]: self.lens_objects[standard_name]['References'].append(sugohi_key[row[-1]])
-                else: self.lens_objects[standard_name]['References'] = [sugohi_key[row[-1]]]
+                for sr in row[-1].split(' '):
+                    if 'SuGOHI' in sr: break
+                if 'References' in self.lens_objects[standard_name]: self.lens_objects[standard_name]['References'].append(sr)
+                else: self.lens_objects[standard_name]['References'] = [sr]
                 
             
            
